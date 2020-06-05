8e1b20baa8bfe6e6b1ca8062a3f87f47fef7f573
anasu <misushima@gmail.com>
2020-06-05 12:48:49 -0400

    Se instala firebase en proyecto (y algunas otras dependencias)

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Firebase from 'firebase'
+import { firebaseConfig } from 'config/firebaseConfig.js'
+// Inicializador de Firebase
+Firebase.initializeApp(firebaseConfig);
+Firebase.analytics();
+// esto me permite exportar la base de datos y jugar con ella en otros archivos
+export const db = Firebase.firestore();
 
 Vue.config.productionTip = false
 
