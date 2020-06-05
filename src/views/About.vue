84b59c216623d3822e06e2046b14f995cb5d6cb2
anasu <misushima@gmail.com>
2020-06-05 16:20:36 -0400

    Se agrega formulario para validar usuario

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,64 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Aquí testearé el login</h1>
+    <div class="formulario">
+      <input 
+        type="email" 
+        placeholder="Ingrese el correo electrónico"
+        v-model="mail">
+      <input 
+        type="password" 
+        placeholder="Ingrese la contraseña"
+        v-model="pass">
+      <button @click="loginUser">Entrar</button>
+    </div>
   </div>
 </template>
+
+<script>
+import Firebase from 'firebase'
+
+export default {
+  name: 'about',
+  data() {
+    return {
+      mail: null,
+      pass: null,
+    }
+  },
+  methods: {
+    loginUser () {
+      Firebase.auth().signInWithEmailAndPassword(
+        this.mail, this.pass
+        ).then(
+          (datos) => {
+            console.log(datos);
+            console.log(datos.user);
+            this.$router.push('./')
+          }
+        ).catch(
+          err => {
+            console.log(err)
+          }
+        )
+    }
+  }
+
+}
+</script>
+
+<style lang="scss" scoped>
+.about {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  
+  .formulario {
+    display: flex;
+    align-items: space-around;
+    flex-direction: column;
+    max-width: 800px;
+  }
+
+}
+</style>
