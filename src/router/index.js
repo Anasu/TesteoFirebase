880ab46f7d08580c56f588b4b9cd403faa715910
anasu <misushima@gmail.com>
2020-06-09 12:24:44 -0400

    Se agrega router guard global para que me mande al home si estoy loggeada

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Firebase from 'firebase'
 
 Vue.use(VueRouter)
 
@@ -40,4 +41,18 @@ const router = new VueRouter({
   routes
 })
 
+router.beforeEach((to, from, next) => {
+  let user = Firebase.auth().currentUser;
+
+  let authRequired = to.matched.some(route => route.meta.login);
+
+  if (!user && authRequired) {
+    next('login');
+  } else if (user && !authRequired) {
+    next('home');
+  } else {
+    next();
+  }
+ });
+ 
 export default router
