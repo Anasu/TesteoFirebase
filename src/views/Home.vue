5a4dc306ab7b5fb7f38ca335c4e9020c60c4b4fe
anasu <misushima@gmail.com>
2020-06-11 11:41:24 -0400

    Se agrega Logout a tag <a>

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <a>Logout</a>
+    <a @click="logout" href="#">Logout</a>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
