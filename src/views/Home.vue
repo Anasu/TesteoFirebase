2948af89b62c713d8aad679aae759ecfde212ddc
anasu <misushima@gmail.com>
2020-06-10 20:52:11 -0400

    Se agrega autentificacion para poder ingresar al home SOLO despues de loggear

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <a>Logout</a>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -8,11 +9,21 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import Firebase from 'firebase'
 
 export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  methods: {
+    logout() {
+      Firebase.auth().signOut()
+        .then(accept => {
+          this.$router.push('login');
+          console.log(accept) //pa que el linter no hinche
+        });
+    }
   }
 }
 </script>
